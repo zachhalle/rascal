b27f5cb0569004ccde2636b43a7e6764dea97fb7
Zach Halle <zach.halle95@gmail.com>
2022-08-27 20:51:03 -0700

    pretty printing ast

--- lib/ast.ml
+++ lib/ast.ml
@@ -10,3 +10,38 @@ type expr =
 type stmt =
   | Define of string * expr
   | Expr of expr
+
+let rec pprint_expr e =
+  let open PPrint in
+  match e with
+  | Int i -> string (string_of_int i)
+  | Float f -> string (string_of_float f)
+  | String s -> enclose (char '\"') (char '\"') (string s)
+  | Bool b -> string (string_of_bool b)
+  | Var v -> string v
+  | Lambda (v, e) -> flow (break 1) [string "(lambda"; string v; pprint_expr e] ^^ string ")"
+  | List es -> enclose lparen rparen (flow_map (break 1) pprint_expr es)
+
+let pprint_stmt s =
+  let open PPrint in
+  match s with
+  | Define (v, e) -> flow (break 1) [string "(define"; string v; pprint_expr e] ^^ string ")"
+  | Expr e -> pprint_expr e
+
+let pprint_prog p =
+  let open PPrint in
+  flow_map hardline pprint_stmt p
+
+let show_document doc =
+  let open PPrint.ToBuffer in
+  let rfrac = 0.85 in
+  let width = 100 in
+  let buffer = Buffer.create 10 in
+  pretty rfrac width buffer doc;
+  Buffer.contents buffer
+
+let pretty_expr e = show_document (pprint_expr e)
+
+let pretty_stmt s = show_document (pprint_stmt s)
+
+let pretty_prog p = show_document (pprint_prog p)
---
+++ lib/ast.mli
@@ -0,0 +1,16 @@
+type expr =
+  | Int of int
+  | Float of float
+  | String of string
+  | Bool of bool
+  | Var of string
+  | Lambda of string * expr
+  | List of expr list
+
+type stmt =
+  | Define of string * expr
+  | Expr of expr
+
+val pretty_expr : expr -> string
+val pretty_stmt : stmt -> string
+val pretty_prog : stmt list -> string
