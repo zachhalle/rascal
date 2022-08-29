ef4cb44519a11a8e717c7147289e01385b43fcb6
Zach Halle <zach.halle95@gmail.com>
2022-08-28 23:18:54 -0700

    eval continued

--- lib/eval.ml
+++ lib/eval.ml
@@ -1,4 +1,6 @@
 open Ast
+open Context
+open Printf
 
 exception Runtime_error of string
 
@@ -9,30 +11,43 @@ let is_value e =
   | Lambda _ -> true
   | List _ -> false
 
-let is_primitive _ = failwith ""
+let is_primitive _ = false
 
 let apply_primitive _ _ = failwith ""
+
+let bind_all context vs es =
+  List.fold_left2 (fun context v e -> bind context v e) context vs es
 
 let rec eval context e =
   match e with
   | Int _ | Float _ | String _
-  | Bool _ | Var _ | Quote _
-  | Lambda _ -> e
-  | List [] -> raise (Runtime_error "Illegal empty application.")
+  | Bool _ | Quote _ | Lambda _ -> e
+  | Var v -> substitute context v
+  | List [] -> raise (Runtime_error "Illegal empty application")
   | List es -> apply context (List.map (eval context) es)
 
-and apply _ es =
+and apply context es =
   match es with
   | [] -> assert false
-  | e :: _ ->
+  | e :: es' ->
     if is_primitive e then
       apply_primitive e es
     else
       match e with
       | Int _ | Float _ | String _
-      | Bool _ | Var _ | Quote _
-      | List _ -> failwith ""
-      | Lambda _ -> failwith ""
+      | Bool _ | Quote _ | List _ ->
+        raise (
+          Runtime_error (
+            sprintf "Tried to apply a value that isn't a procedure: %s" (pretty_expr e)))
+      | Var v -> apply context (substitute context v :: es')
+      | Lambda (vs, e') ->
+        let nvs = List.length vs in
+        let nes = List.length es' in
+        if nvs != nes then
+          raise (Runtime_error (sprintf "Arity mismatch: found %d arguments but expected %d" nvs nes))
+        else
+          eval (bind_all context vs es') e'
+            
 
 let eval_stmt _ = failwith ""
 
